5d0e75fd724af36fcbece9411c07c5d333f1ce2c
Jonathan Gravel <jo@stashed.dev>
2022-10-19 23:33:55 -0400

    Fix rustup configuration

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -205,14 +205,41 @@ lvim.plugins = {
   },
   {
     "simrat39/rust-tools.nvim",
+    -- ft = { "rust", "rs" },
     config = function()
-      local lsp_installer_servers = require("nvim-lsp-installer.servers")
-      local _, requested_server = lsp_installer_servers.get_server("rust_analyzer")
-      local opts = {
+      require("rust-tools").setup {
+        tools = {
+          executor = require("rust-tools/executors").termopen, -- can be quickfix or termopen
+          reload_workspace_from_cargo_toml = true,
+          inlay_hints = {
+            auto = true,
+            only_current_line = false,
+            show_parameter_hints = true,
+            parameter_hints_prefix = "<-",
+            other_hints_prefix = "=>",
+            max_len_align = false,
+            max_len_align_padding = 1,
+            right_align = false,
+            right_align_padding = 7,
+            highlight = "Comment",
+          },
+          hover_actions = {
+            border = {
+              { "╭", "FloatBorder" },
+              { "─", "FloatBorder" },
+              { "╮", "FloatBorder" },
+              { "│", "FloatBorder" },
+              { "╯", "FloatBorder" },
+              { "─", "FloatBorder" },
+              { "╰", "FloatBorder" },
+              { "│", "FloatBorder" },
+            },
+            auto_focus = true,
+          },
+        },
         server = {
-          cmd = requested_server._default_options.cmd,
-          on_attach = require("lvim.lsp").common_on_attach,
           on_init = require("lvim.lsp").common_on_init,
+          on_attach = require("lvim.lsp").common_on_attach,
           settings = {
             ["rust-analyzer"] = {
               checkOnSave = {
@@ -225,9 +252,7 @@ lvim.plugins = {
           },
         },
       }
-      require("rust-tools").setup(opts)
     end,
-    ft = { "rust", "rs" },
   },
   {
     "norcalli/nvim-colorizer.lua",
