9743d954e4cbe86015277da4bbc2f44f1e7a7985
Jonathan Gravel <jo@stashed.dev>
2024-01-20 22:53:42 -0500

    Config sync

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -105,7 +105,7 @@ lvim.builtin.treesitter.highlight.enable = true
 -- ---@usage disable automatic installation of servers
 -- lvim.lsp.installer.setup.automatic_installation = false
 
--- Rust LSP (setup manually via rust-tools)
+-- Rust LSP (setup manually via rustaceanvim)
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "rust_analyzer" })
 
 -- HTML/CSS LSP
@@ -140,7 +140,7 @@ local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
   { command = "black", filetypes = { "python" } },
   { command = "isort", filetypes = { "python" } },
-  { command = "leptosfmt", filetypes = { "rust" } },
+  -- { command = "leptosfmt", filetypes = { "rust" } },
 }
 
 -- Additional Plugins
@@ -160,57 +160,93 @@ lvim.plugins = {
     end
   },
   {
-    "simrat39/rust-tools.nvim",
-    -- ft = { "rust", "rs" },
+    "lvimuser/lsp-inlayhints.nvim",
     config = function()
-      require("rust-tools").setup {
-        tools = {
-          executor = require("rust-tools/executors").termopen, -- can be quickfix or termopen
-          reload_workspace_from_cargo_toml = true,
-          inlay_hints = {
-            auto = true,
-            only_current_line = false,
-            show_parameter_hints = true,
-            parameter_hints_prefix = "<- ",
-            other_hints_prefix = "=> ",
-            max_len_align = false,
-            max_len_align_padding = 1,
-            right_align = false,
-            right_align_padding = 7,
-            highlight = "Comment",
-          },
-          hover_actions = {
-            border = {
-              { "╭", "FloatBorder" },
-              { "─", "FloatBorder" },
-              { "╮", "FloatBorder" },
-              { "│", "FloatBorder" },
-              { "╯", "FloatBorder" },
-              { "─", "FloatBorder" },
-              { "╰", "FloatBorder" },
-              { "│", "FloatBorder" },
-            },
-            auto_focus = true,
-          },
-        },
+      require("lsp-inlayhints").setup()
+    end
+  },
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^3",
+    ft = { "rust" },
+    config = function()
+      vim.g.rustaceanvim = {
         server = {
           on_init = require("lvim.lsp").common_on_init,
-          on_attach = require("lvim.lsp").common_on_attach,
+          on_attach = function(client, bufnr)
+            require("lsp-inlayhints").on_attach(client, bufnr)
+            require("lvim.lsp").common_on_attach(client, bufnr)
+          end,
           settings = {
             ["rust-analyzer"] = {
               checkOnSave = {
                 command = "clippy",
                 allTargets = true,
-                features = "all",
                 loadOutDirsFromCheck = true,
+                features = "all",
                 -- command = "cargo clippy --workspace --message-format=json --all-targets --all-features"
               },
+              cargo = {
+                allFeatures = true,
+              }
             },
           },
         },
       }
     end,
   },
+  -- {
+  --   "simrat39/rust-tools.nvim",
+  --   -- ft = { "rust", "rs" },
+  --   config = function()
+  --     require("rust-tools").setup {
+  --       tools = {
+  --         executor = require("rust-tools/executors").termopen, -- can be quickfix or termopen
+  --         reload_workspace_from_cargo_toml = true,
+  --         inlay_hints = {
+  --           auto = true,
+  --           only_current_line = false,
+  --           show_parameter_hints = true,
+  --           parameter_hints_prefix = "<- ",
+  --           other_hints_prefix = "=> ",
+  --           max_len_align = false,
+  --           max_len_align_padding = 1,
+  --           right_align = false,
+  --           right_align_padding = 7,
+  --           highlight = "Comment",
+  --         },
+  --         hover_actions = {
+  --           border = {
+  --             { "╭", "FloatBorder" },
+  --             { "─", "FloatBorder" },
+  --             { "╮", "FloatBorder" },
+  --             { "│", "FloatBorder" },
+  --             { "╯", "FloatBorder" },
+  --             { "─", "FloatBorder" },
+  --             { "╰", "FloatBorder" },
+  --             { "│", "FloatBorder" },
+  --           },
+  --           auto_focus = true,
+  --         },
+  --       },
+  --       server = {
+  --         on_init = require("lvim.lsp").common_on_init,
+  --         on_attach = require("lvim.lsp").common_on_attach,
+  --         settings = {
+  --           ["rust-analyzer"] = {
+  --             checkOnSave = {
+  --               command = "clippy",
+  --               allTargets = true,
+  --               features = "all",
+  --               loadOutDirsFromCheck = true,
+  --               -- command = "cargo clippy --workspace --message-format=json --all-targets --all-features"
+  --             },
+  --           },
+  --         },
+  --       },
+  --     }
+  --   end,
+  -- },
   {
     "norcalli/nvim-colorizer.lua",
     config = function()
