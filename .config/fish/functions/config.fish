4832194a0156aa86baeaa805316da89fd2be17e8
Jonathan Gravel <jo@stashed.dev>
2024-01-27 15:49:18 -0500

    fish: fix config plugin for good

--- .config/fish/functions/config.fish
+++ .config/fish/functions/config.fish
@@ -1,3 +1,3 @@
-function config --wraps='/usr/bin/git --git-dir=$HOME/.cfg/.git --work-tree=$HOME' --description 'alias config=/usr/bin/git --git-dir=$HOME/.cfg/.git --work-tree=$HOME'
-  /usr/bin/git --git-dir=$HOME/.cfg/.git --work-tree=$HOME $argv
+function config --wraps='/usr/bin/git --git-dir=$HOME/.cfg --work-tree=$HOME' --description 'alias config=/usr/bin/git --git-dir=$HOME/.cfg --work-tree=$HOME'
+  /usr/bin/git --git-dir=$HOME/.cfg --work-tree=$HOME $argv
 end
