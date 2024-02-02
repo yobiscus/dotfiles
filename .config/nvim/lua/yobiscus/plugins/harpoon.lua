bbf57c9966bc0dc28921dbcbc2250715ba284413
Jonathan Gravel <jogravel@cisco.com>
2024-02-02 10:17:04 -0500

    neovim: update harpoon keybinds

--- .config/nvim/lua/yobiscus/plugins/harpoon.lua
+++ .config/nvim/lua/yobiscus/plugins/harpoon.lua
@@ -4,12 +4,12 @@ return {
     local mark = require("harpoon.mark")
     local ui = require("harpoon.ui")
 
-    vim.keymap.set('n', '<leader>a', mark.add_file)
-    vim.keymap.set('n', '<C-s>', ui.toggle_quick_menu)
+    vim.keymap.set('n', '<leader>oa', mark.add_file, { desc = "Harpoon file" })
+    vim.keymap.set('n', '<leader>oh', ui.toggle_quick_menu, { desc = "Harpoon list" })
 
     -- optimized for colemak keybind even though I am on qwerty
-    vim.keymap.set('n', '<C-m>', function() ui.nav_file(1) end) -- netrw hijacks this, what to do?
+    vim.keymap.set('n', '<leader>oq', function() ui.nav_file(1) end, { desc = "Harpoon file 1" }) -- netrw hijacks this, what to do?
     -- vim.keymap.set('n', '<C-n>', function() ui.nav_file(2) end)
-    vim.keymap.set('n', '<C-e>', function() ui.nav_file(2) end)
+    vim.keymap.set('n', '<leader>ow', function() ui.nav_file(2) end, { desc = "Harpoon file 2" })
   end
 }
