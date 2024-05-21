96d83c351c20d8d07eea85201c03fb4e7fb86b2b
Jonathan Gravel <jogravel@cisco.com>
2024-05-21 12:07:33 -0400

    fish: fallback to id_rsa if id_ed25519 is missing

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,11 @@
 if status is-interactive
     bind \cd delete-char  # don't exit on <C-d>
-    keychain --eval id_ed25519 | source
+
+    if test -f $HOME/.ssh/id_ed25519.pub
+        keychain --eval id_ed25519 | source
+    else
+        keychain --eval id_rsa | source
+    end
 end
 
 # tools
