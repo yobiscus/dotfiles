071977bdf91da299874dca55eb6a4aefd92d90a5
Jonathan Gravel <jo@stashed.dev>
2024-11-02 17:34:26 -0400

    i3/kmonad: change i3 buttons when switching keyboard layout

---
+++ .config/i3/swplyt.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Swap i3 configuration to specified keyboard layout {qwerty|colemak}
+#
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+fatal() {
+    echo "Error: $*" >&2
+    exit 1
+}
+
+main() {
+    local layout=$1
+    local file
+
+    case "$layout" in
+        q|qwerty)   file=qwerty.config     ;;
+        c|colemak*) file=colemak.config    ;;
+        *)          fatal "Unexpected layout: $layout" ;;
+    esac
+
+    ln -sf "$file" "$SCRIPT_DIR/config"
+}
+
+main "$@"
