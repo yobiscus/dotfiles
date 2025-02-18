76e76840c2c19c310affd07acad9ef672457b5d5
Jonathan Gravel <jo@stashed.dev>
2025-02-17 20:00:23 -0500

    nvim: disable automatic copilot suggestions

--- .config/nvim/lua/yobiscus/plugins/copilot.lua
+++ .config/nvim/lua/yobiscus/plugins/copilot.lua
@@ -28,7 +28,6 @@ return {
       require('copilot').setup(opts)
 
       local function set_trigger(trigger)
-        vim.b.copilot_suggestion_auto_trigger = trigger
         vim.b.copilot_suggestion_hidden = not trigger
       end
 
