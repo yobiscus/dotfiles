30c90b7f3aa0fa58b4cb30bf165cbb77fa9e2303
Jonathan Gravel <jogravel@cisco.com>
2025-09-08 09:42:38 -0400

    nvim: replace nvim.cmp with blink.cmp

--- .config/nvim/lua/yobiscus/plugins/ai.lua
+++ .config/nvim/lua/yobiscus/plugins/ai.lua
@@ -82,35 +82,5 @@ return {
         yaml = true,
       },
     },
-    config = function(_, opts)
-      local cmp = require 'cmp'
-      local copilot = require 'copilot.suggestion'
-      local luasnip = require 'luasnip'
-
-      require('copilot').setup(opts)
-
-      local function set_trigger(trigger)
-        vim.b.copilot_suggestion_hidden = not trigger
-      end
-
-      -- Hide suggestions when the completion menu is open.
-      cmp.event:on('menu_opened', function()
-        if copilot.is_visible() then
-          copilot.dismiss()
-        end
-        set_trigger(false)
-      end)
-
-      -- Disable suggestions when inside a snippet.
-      cmp.event:on('menu_closed', function()
-        set_trigger(not luasnip.expand_or_locally_jumpable())
-      end)
-      vim.api.nvim_create_autocmd('User', {
-        pattern = { 'LuasnipInsertNodeEnter', 'LuasnipInsertNodeLeave' },
-        callback = function()
-          set_trigger(not luasnip.expand_or_locally_jumpable())
-        end,
-      })
-    end,
   },
 }
--- .config/nvim/lua/yobiscus/plugins/lsp.lua
+++ .config/nvim/lua/yobiscus/plugins/lsp.lua
@@ -3,14 +3,13 @@ return {
   dependencies = {
     { 'williamboman/mason.nvim' },
     { 'neovim/nvim-lspconfig' },
-    { 'hrsh7th/cmp-nvim-lsp' },
-    { 'hrsh7th/nvim-cmp' },
-    { 'L3MON4D3/LuaSnip' },
-    { 'saadparwaiz1/cmp_luasnip' },
-    { 'onsails/lspkind.nvim' },
+    { 'saghen/blink.cmp' },
   },
   config = function()
-    vim.diagnostic.config({ virtual_lines = true })
+    local lspconfig = require('lspconfig')
+
+    -- vim.diagnostic.config({ virtual_lines = true })
+    vim.diagnostic.config({ virtual_text = true })
     vim.keymap.set('n', '<leader>li', function()
       vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
     end, { desc = "toggle inlay hints" })
@@ -18,128 +17,53 @@ return {
     vim.keymap.set(
       'n', 'gl', vim.diagnostic.open_float, { desc = "open diagnostic" })
 
-    local cmp = require('cmp')
-    local luasnip = require('luasnip')
+    -- -- Inside a snippet, use backspace to remove the placeholder.
+    -- vim.keymap.set('s', '<BS>', '<C-O>s')
 
-    -- Inside a snippet, use backspace to remove the placeholder.
-    vim.keymap.set('s', '<BS>', '<C-O>s')
+    local server_opts = {
+      clangd = {},
+      lua_la = {},
+      rust_analyzer = {
+        settings = {
+          ['rust-analyzer'] = {
+            cargo = {
+              allFeatures = true,
+            },
+            checkOnSave = {
+              command = "clippy",
+            },
+            inlayHints = {
+              parameterHints = { enable = true },
+              typeHints = { enable = true },
+              implicitDrops = { enable = true },
+            },
+            procMacro = {
+              ignored = {
+                leptos_macro = {
+                  "server",
+                },
+              },
+            },
+          }
+        }
+      },
+    }
 
-    cmp.setup({
-      -- Disable preselect. On enter, the first thing will be used if nothing
-      -- is selected.
-      preselect = cmp.PreselectMode.None,
-      -- Add icons to the completion menu.
-      formatting = {
-        fields = { 'menu', 'abbr', 'kind' },
-        format = require('lspkind').cmp_format({
-          mode          = 'symbol',
-          maxwidth      = 50,
-          ellipsis_char = '...',
-          menu          = {
-            nvim_lsp = 'λ',
-            luasnip = '⋗',
-            buffer = 'Ω',
-            path = '🖫',
-            nvim_lua = 'Π',
-          },
-          symbol_map    = {
-            Copilot = "",
-          },
-        }),
-      },
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      },
-      view = {
-        -- Explicitly request documentation.
-        -- docs = { auto_open = false },
-      },
-      mapping = cmp.mapping.preset.insert {
-        ['<CR>'] = cmp.mapping.confirm {
-          behavior = cmp.ConfirmBehavior.Replace,
-          -- select = true,
-        },
-        -- Explicitly request completions.
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-/>'] = cmp.mapping.close(),
-        -- Overload tab to accept Copilot suggestions.
-        ['<Tab>'] = cmp.mapping(function(fallback)
-          local copilot = require 'copilot.suggestion'
-
-          if copilot.is_visible() then
-            copilot.accept()
-          elseif cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.expand_or_locally_jumpable() then
-            luasnip.expand_or_jump()
-          else
-            fallback()
-          end
-        end, { 'i', 's' }),
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.expand_or_locally_jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { 'i', 's' }),
-        ['<C-d>'] = function()
-          if cmp.visible_docs() then
-            cmp.close_docs()
-          else
-            cmp.open_docs()
-          end
-        end,
-      },
-      sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
-        { name = 'crates' },
-      }, {
-        { name = 'buffer' },
-      }),
-      window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-      },
-    })
+    -- augment LSP capabilities with blink.cmp
+    local capabilities = require('blink.cmp').get_lsp_capabilities()
+    local server_handlers = {}
+    for server, config in pairs(server_opts) do
+        server_handlers[server] = function()
+            config.capabilities = capabilities
+            lspconfig[server].setup(config)
+        end
+    end
 
     -- https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
     require('mason').setup({})
     require('mason-lspconfig').setup({
       ensure_installed = { 'clangd', 'lua_ls', 'rust_analyzer' },
-      handlers = {
-        rust_analyzer = function()
-          require('lspconfig').rust_analyzer.setup({
-            settings = {
-              ['rust-analyzer'] = {
-                cargo = {
-                  allFeatures = true,
-                },
-                checkOnSave = {
-                  command = "clippy",
-                },
-                inlayHints = {
-                  parameterHints = { enable = true },
-                  typeHints = { enable = true },
-                  implicitDrops = { enable = true },
-                },
-                procMacro = {
-                  ignored = {
-                    leptos_macro = {
-                      "server",
-                    },
-                  },
-                },
-              }
-            }
-          })
-        end,
-      },
+      handlers = server_handlers
     })
   end
 }
